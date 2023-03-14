66940c23c87586eccbf31253cc1696069f078cd9
Mohammed Gomaa <mohammedgomaa2522@gmail.com>
2023-03-14 09:24:36 +0200

    Update Design

--- js/main.js
+++ js/main.js
@@ -13,3 +13,60 @@ const showNav = () => {
 };
 showNav();
 // ---------------------------------------------------------
+// Gallery
+const gallery = () => {
+    const liG = document.querySelectorAll(".lang li"),
+        pro = document.querySelectorAll(".gallery .pro");
+
+    // Event
+    liG.forEach((ev) => {
+        ev.addEventListener("click", () => {
+            // Remove class Name from btn 
+            liG.forEach((ev) => {
+                ev.classList.remove("active");
+            });
+            // Add class Name to btn 
+            ev.classList.add("active");
+            // Add Element to gallery  
+            pro.forEach((ele) => {
+                if (ele.classList.contains(`${ev.id}`)) {
+                    ele.style.height = "185px";
+                } else {
+                    ele.style.height = "0";
+                }
+            });
+        });
+    });
+};
+gallery();
+// ---------------------------------------------------------
+// toggle options
+const options = () => {
+    // Start Show Options
+    const opt = document.querySelector(".options"),
+        tgl = document.querySelector(".options .toggle-btn");
+    // -----------
+    
+    tgl.addEventListener("click", () => {
+        opt.classList.toggle("active");
+    });
+    // End Show Options
+    // -----------
+    // Start Change color
+    const root = document.querySelector(':root'),
+        color = document.querySelectorAll(".options .colors .color");
+    // -----------
+
+    color.forEach((ev) => {
+        ev.addEventListener("click", () => {
+            // Change Main color variable
+            root.style.setProperty('--main-color', ev.id)
+            window.localStorage.setItem("color", ev.id);
+        });
+    });
+    if (localStorage.getItem("color")) {
+        root.style.setProperty('--main-color', localStorage.getItem("color"));
+    }
+    // End Change color
+};
+options();
